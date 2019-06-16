75f246060e9d6d3aacecbab63b8eb411c39d8b37
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-06-16 18:11:36 +0200

    add bootstrap

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import './styles/Badge.css';
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src="https://www.gravatar.com/avatar?d=identicon" alt="Logo"/>
         </div>
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"/>
+        <div className="Badge__section-name">
+          <img className="Badge__avatar" src="https://media.licdn.com/dms/image/C4E03AQHt7VrSl95qng/profile-displayphoto-shrink_200_200/0?e=1566432000&v=beta&t=2b3JrV9OSjt-84_K6WiVRF4L5DMMYmoHRW2rCNWVUNo" alt="Avatar"/>
           <h1>
             Gian <br/> Pumayalla
           </h1>
         </div>
-        <div>
+        <div className="Badge__section-info">
           <p>Full Stack developer</p>
+        </div>
+        <div className="Badge__footer">
+          #Italy
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
 import Badge from './components/Badge.jsx';
 
 const container = document.getElementById('root')
