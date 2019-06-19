d411e5fcc42c0eb5d854f89656a26dd1a0d87260
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-06-19 23:10:03 +0200

    lifecycle

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -12,40 +12,67 @@ class Badges extends React.Component {
     console.log('1.constructor()')
 
     this.state = {
-      data: [
-        {
-          id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-          firstName: 'Freda',
-          lastName: 'Grady',
-          email: 'Leann_Berge@gmail.com',
-          jobTitle: 'Legacy Brand Director',
-          avatar:
-            'https://dummyimage.com/64x64/000/fff',
-        },
-        {
-          id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-          firstName: 'Major',
-          lastName: 'Rodriguez',
-          email: 'Ilene66@hotmail.com',
-          jobTitle: 'Human Research Architect',
-          avatar:
-            'https://dummyimage.com/64x64/000/fff',
-        },
-        {
-          id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-          firstName: 'Daphney',
-          lastName: 'Torphy',
-          email: 'Ron61@hotmail.com',
-          jobTitle: 'National Markets Officer',
-          avatar:
-            'https://dummyimage.com/64x64/000/fff',
-        },
-      ],
+      data: [],
     };
   }
 
   componentDidMount(){
     console.log('3.componentDidMount()')
+
+    this.timeoutId = setTimeout(()=>{
+      this.setState({
+        data: [
+          {
+            id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+            firstName: 'Freda',
+            lastName: 'Grady',
+            email: 'Leann_Berge@gmail.com',
+            jobTitle: 'Legacy Brand Director',
+            avatar:
+              'https://dummyimage.com/64x64/000/fff',
+          },
+          {
+            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+            firstName: 'Major',
+            lastName: 'Rodriguez',
+            email: 'Ilene66@hotmail.com',
+            jobTitle: 'Human Research Architect',
+            avatar:
+              'https://dummyimage.com/64x64/000/fff',
+          },
+          {
+            id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+            firstName: 'Daphney',
+            lastName: 'Torphy',
+            email: 'Ron61@hotmail.com',
+            jobTitle: 'National Markets Officer',
+            avatar:
+              'https://dummyimage.com/64x64/000/fff',
+          },
+        ],
+      })
+    },3000)
+
+    
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log('5.ComponentDidUpdate()');
+    console.log({
+      prevProps: prevProps, 
+      prevState: prevState,
+    });
+
+    console.log({
+      props: this.props, 
+      state: this.state,
+    });
+  }
+
+  componentWillUnmount(){
+    console.log('6.ComponentWillUnmount');
+
+    clearTimeout(this.timeoutId);
   }
 
   render() {
