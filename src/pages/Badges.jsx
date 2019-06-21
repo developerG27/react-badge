390ae5f23dbf0b4fbfa465a1f401dc9c37189c69
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-06-21 15:34:14 +0200

    add api

--- src/pages/Badges.jsx
+++ src/pages/Badges.jsx
@@ -5,78 +5,40 @@ import '../components/styles/Badges.css';
 // import Navbar from '../components/Navbar';
 import BadgesList from '../components/BadgesList';
 
+import api from '../api';
+
 class Badges extends React.Component {
 
-  constructor(props){
-    super(props);
-    console.log('1.constructor()')
-
-    this.state = {
-      data: [],
-    };
-  }
+  state = {
+    loading: true,
+    error: null,
+    data: undefined
+  };
 
   componentDidMount(){
-    console.log('3.componentDidMount()')
-
-    this.timeoutId = setTimeout(()=>{
-      this.setState({
-        data: [
-          {
-            id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-            firstName: 'Freda',
-            lastName: 'Grady',
-            email: 'Leann_Berge@gmail.com',
-            jobTitle: 'Legacy Brand Director',
-            avatar:
-              'https://dummyimage.com/64x64/000/fff',
-          },
-          {
-            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-            firstName: 'Major',
-            lastName: 'Rodriguez',
-            email: 'Ilene66@hotmail.com',
-            jobTitle: 'Human Research Architect',
-            avatar:
-              'https://dummyimage.com/64x64/000/fff',
-          },
-          {
-            id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-            firstName: 'Daphney',
-            lastName: 'Torphy',
-            email: 'Ron61@hotmail.com',
-            jobTitle: 'National Markets Officer',
-            avatar:
-              'https://dummyimage.com/64x64/000/fff',
-          },
-        ],
-      })
-    },3000)
-
-    
+    this.fetchData()    
   }
 
-  componentDidUpdate(prevProps, prevState){
-    console.log('5.ComponentDidUpdate()');
-    console.log({
-      prevProps: prevProps, 
-      prevState: prevState,
-    });
+  fetchData = async () => {
+    this.setState({loading: true, error: null})
 
-    console.log({
-      props: this.props, 
-      state: this.state,
-    });
-  }
-
-  componentWillUnmount(){
-    console.log('6.ComponentWillUnmount');
-
-    clearTimeout(this.timeoutId);
+    try{
+      const data = await api.badges.list();
+      this.setState({loading: false, data: data})
+    } catch(error){
+      this.setState({loading: false, error: error})
+    }
   }
 
   render() {
-    console.log('2.render()')
+    if(this.state.loading === true){
+      return 'Loading..';
+    }
+
+    if(this.state.error){
+      return `Error..${this.state.error.message}`;
+    }
+
     return (
       <div>
         
